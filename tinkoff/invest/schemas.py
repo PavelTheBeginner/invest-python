8e1d29e40176fecf91a6e1e4a681fbd052e8a596
Pavel <91403910+PavelTheBeginner@users.noreply.github.com>
2025-05-16 00:34:04 +0300

    Fix ReplaceOrderRequest to be dataclass

--- tinkoff/invest/schemas.py
+++ tinkoff/invest/schemas.py
@@ -2385,7 +2385,7 @@ class OrderStage(_grpc_helpers.Message):
     trade_id: str = _grpc_helpers.string_field(3)
     execution_time: datetime = _grpc_helpers.message_field(5)
 
-
+@dataclass(eq=False, repr=True)
 class ReplaceOrderRequest(_grpc_helpers.Message):
     account_id: str = _grpc_helpers.string_field(1)
     order_id: str = _grpc_helpers.string_field(6)
